a2fdffa2b7bcb7d3d904c62bea0aac864cd2aeab
HDLC <henri.libault-de-la-chevasnerie@fortil.group>
2025-01-10 10:43:53 +0100

    init project from zip

---
+++ Controllers/LivresController.cs
@@ -0,0 +1,172 @@
+﻿#nullable disable
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models.BO;
+
+namespace Web.Controllers
+{
+    public class LivresController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LivresController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+        }
+
+        // GET: Livres
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Livres.ToListAsync());
+        }
+
+        // GET: Livres/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livre = await _context.Livres
+                .FirstOrDefaultAsync(m => m.LivreId == id);
+            if (livre == null)
+            {
+                return NotFound();
+            }
+
+            return View(livre);
+        }
+
+        // GET: Livres/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Livres/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LivreId,Content,CreatedDate,Status,UserId")] Livre livre)
+        {
+            if (ModelState.IsValid)
+            {
+                livre.UserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                _context.Add(livre);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livre);
+        }
+
+        // GET: Livres/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livre = await _context.Livres.FindAsync(id);
+            if (livre == null)
+            {
+                return NotFound();
+            }
+            return View(livre);
+        }
+
+        // POST: Livres/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LivreId,Content,CreatedDate,Status,UserId")] Livre livre)
+        {
+            if (id != livre.LivreId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(livre);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LivreExists(livre.LivreId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livre);
+        }
+
+        // GET: Livres/Delete/5
+        [Authorize(Roles = "administrator")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livre = await _context.Livres
+                .FirstOrDefaultAsync(m => m.LivreId == id);
+            if (livre == null)
+            {
+                return NotFound();
+            }
+
+            return View(livre);
+        }
+
+        // POST: Livres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var livre = await _context.Livres.FindAsync(id);
+            _context.Livres.Remove(livre);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ApproveOrReject(int id, LivreStatus status)
+        {
+            var livre = await _context.Livres.FirstOrDefaultAsync(
+                                          m => m.LivreId == id);
+
+            if (livre == null)
+            {
+                return NotFound();
+            }
+
+            livre.Status = status;
+            _context.Livres.Update(livre);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LivreExists(int id)
+        {
+            return _context.Livres.Any(e => e.LivreId == id);
+        }
+    }
+}
